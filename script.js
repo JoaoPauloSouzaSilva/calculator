fd40c791db35195fb23da998087bf9156b6143df
JoaoPauloSouzaSilva <joaopaulloss477@gmail.com>
2024-07-24 10:49:54 -0300

    Clearing Comment

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function btn_AC() {
 
   txt_num.innerHTML = "";
   txt_result.innerHTML = "";
-}
+};
 
 function btn_parentesis() {
   if (parenteseIs) {
@@ -33,132 +33,115 @@ function btn_parentesis() {
   } else {
     array.push(")");
     parenteseIs = true;
-  }
+  };
   percorreArray();
-}
+};
 
 function btn_porcentagem() {
   if (array.length > 0 && typeof array[array.length - 1] === "number") {
     array.push("%");
-  }
+  };
   percorreArray();
-}
+};
 
 function btn_divisao() {
   if (array.length > 0 && typeof array[array.length - 1] === "number") {
     array.push("÷");
-  }
+  };
   percorreArray();
-}
+};
 
 function btn_multiplicacao() {
   if (array.length > 0 && typeof array[array.length - 1] === "number") {
     array.push("x");
-  }
+  };
   percorreArray();
-}
+};
 
 function btn_subtracao() {
-  // if ( array.length === 0 || typeof array[array.length - 1] === "number" || array[array.length - 1] === 'x') {
-  //   array.push("-");
-  // };
   if (array.length > 0 && typeof array[array.length - 1] === "number") {
     array.push("-");
-  }
+  };
 
   percorreArray();
-}
+};
 
 function btn_soma() {
   if (array.length > 0 && typeof array[array.length - 1] === "number") {
     array.push("+");
-  }
+  };
   percorreArray();
-}
+};
 
 function btn_virgula() {
   if (array.length > 0 && typeof array[array.length - 1] === "number") {
     array.push(",");
-  }
+  };
   percorreArray();
-}
+};
 
 function btn_0() {
   array.push(0);
   percorreArray();
-}
+};
 
 function btn_1() {
   array.push(1);
   percorreArray();
-}
+};
 
 function btn_2() {
   array.push(2);
   percorreArray();
-}
+};
 
 function btn_3() {
   array.push(3);
   percorreArray();
-}
+};
 
 function btn_4() {
   array.push(4);
   percorreArray();
-}
+};
 
 function btn_5() {
   array.push(5);
   percorreArray();
-}
+};
 
 function btn_6() {
   array.push(6);
   percorreArray();
-}
+};
 
 function btn_7() {
   array.push(7);
   percorreArray();
-}
+};
 
 function btn_8() {
   array.push(8);
   percorreArray();
-}
+};
 
 function btn_9() {
   array.push(9);
   percorreArray();
-}
+};
 
 function btn_igual() {
   array.push("=");
-  // if (typeof array[array.length - 2] == "number") {
   AgrupaNumeros();
   IterarArrayNum();
-  // };
-
-  // if (ArryaNum.indexOf("x") != -1) {
-  //   if (typeof ArryaNum[ArryaNum.indexOf("x") - 1] == "number") {
-  //     ArrayMulti.push(Number(ArryaNum[ArryaNum.indexOf("x") - 1]));
-  //   }
-
-  //   if (typeof ArryaNum[ArryaNum.indexOf("x") + 1] == "number") {
-  //     ArrayMulti.push(Number(ArryaNum[ArryaNum.indexOf("x") + 1]));
-  //   }
-
-  //   Multi(ArryaNum.indexOf("x"));
-  // };
-}
+};
 
 function percorreArray() {
   txt_num.innerHTML = "";
   array.forEach(function (caracter) {
     txt_num.innerHTML += caracter;
   });
-}
+};
 
 function AgrupaNumeros() {
   array.forEach(function (caracter) {
@@ -175,15 +158,9 @@ function AgrupaNumeros() {
     }
   });
   array.length = 0;
-}
+};
 
 function IterarArrayNum() {
-  // ArryaNum.forEach(function (value, index) {
-  //     if (value === 'x') {
-  //       LogicaMulti(index);
-  //     };
-  // });
-
   for (let i = 0; i <= ArryaNum.length; i++) {
     if (ArryaNum[i] == "x") {
       LogicaMulti(i);
@@ -191,8 +168,8 @@ function IterarArrayNum() {
       LogicaDivisao(i);
     } else if (ArryaNum[i] == "%") {
       LogicaPorcentagem(i);
-    }
-  }
+    };
+  };
 
   ArryaNum.forEach(function (value, index) {
     if (typeof value === "string") {
@@ -214,35 +191,21 @@ function IterarArrayNum() {
     }
   });
   ArryaNum.length = 0;
-}
+};
 
 function LogicaPorcentagem(index) {
-  // if (ArryaNum.indexOf("x") == 1) {
+
   if (typeof ArryaNum[index - 1] == "number") {
     ArrayPorcentagem.push(Number(ArryaNum[index - 1]));
-  }
+  };
 
   if (typeof ArryaNum[index + 1] == "number") {
     ArrayPorcentagem.push(Number(ArryaNum[index + 1]));
-  }
-  // else{
-  //   ArryaNum.splice(index + 1, 1);
-  //   ArrayMulti.push(Number(ArryaNum[index + 1]) * -1);
-
-  // };
+  };
 
   Porcentagem(index);
   IterarArrayNum();
-
-  //ArryaNum.includes('x') se tiver x no array retorna true
-
-  // } else {
-  //   if (typeof ArryaNum[ArryaNum.indexOf("x") + 1] == "number") {
-  //     ArraySoma.push(Number(ArryaNum[ArryaNum.indexOf("x") + 1]));
-  //   };
-  //   Multi();
-  // };
-}
+};
 
 function Porcentagem(index) {
   let counter = ArrayPorcentagem[0];
@@ -263,163 +226,97 @@ function Porcentagem(index) {
 };
 
 function LogicaDivisao(index) {
-  // if (ArryaNum.indexOf("x") == 1) {
   if (typeof ArryaNum[index - 1] == "number") {
     ArrayDivisao.push(Number(ArryaNum[index - 1]));
-  }
+  };
 
   if (typeof ArryaNum[index + 1] == "number") {
     ArrayDivisao.push(Number(ArryaNum[index + 1]));
-  }
-  // else{
-  //   ArryaNum.splice(index + 1, 1);
-  //   ArrayMulti.push(Number(ArryaNum[index + 1]) * -1);
-
-  // };
+  };
 
   Divisao(index);
   IterarArrayNum();
-
-  //ArryaNum.includes('x') se tiver x no array retorna true
-
-  // } else {
-  //   if (typeof ArryaNum[ArryaNum.indexOf("x") + 1] == "number") {
-  //     ArraySoma.push(Number(ArryaNum[ArryaNum.indexOf("x") + 1]));
-  //   };
-  //   Multi();
-  // };
-}
+};
 
 function Divisao(index) {
   let counter = ArrayDivisao[0];
   for (let i = 1; i < ArrayDivisao.length; i++) {
     counter /= ArrayDivisao[i];
-  }
-  // if (IndexArrayNum == 1) {
+  };
+
   ArryaNum.splice(index - 1, 2);
   ArryaNum[index - 1] = counter;
   ArrayDivisao.length = 0;
-  // } else {
-  //   ArryaNum.splice(IndexArrayNum - 1, 2);
-  //   ArryaNum[IndexArrayNum - 1] = counter;
-  //   ArrayMulti.length = 0;
-  // };
-}
+};
 
 function LogicaMulti(index) {
-  // if (ArryaNum.indexOf("x") == 1) {
   if (typeof ArryaNum[index - 1] == "number") {
     ArrayMulti.push(Number(ArryaNum[index - 1]));
-  }
+  };
 
   if (typeof ArryaNum[index + 1] == "number") {
     ArrayMulti.push(Number(ArryaNum[index + 1]));
-  }
-  // else{
-  //   ArryaNum.splice(index + 1, 1);
-  //   ArrayMulti.push(Number(ArryaNum[index + 1]) * -1);
-
-  // };
+  };
 
   Multi(index);
   IterarArrayNum();
-
-  //ArryaNum.includes('x') se tiver x no array retorna true
-
-  // } else {
-  //   if (typeof ArryaNum[ArryaNum.indexOf("x") + 1] == "number") {
-  //     ArraySoma.push(Number(ArryaNum[ArryaNum.indexOf("x") + 1]));
-  //   };
-  //   Multi();
-  // };
-}
+};
 
 function Multi(index) {
   let counter = ArrayMulti[0];
   for (let i = 1; i < ArrayMulti.length; i++) {
     counter *= ArrayMulti[i];
-  }
-  // if (IndexArrayNum == 1) {
+  };
+
   ArryaNum.splice(index - 1, 2);
   ArryaNum[index - 1] = counter;
   ArrayMulti.length = 0;
-  // } else {
-  //   ArryaNum.splice(IndexArrayNum - 1, 2);
-  //   ArryaNum[IndexArrayNum - 1] = counter;
-  //   ArrayMulti.length = 0;
-  // };
-}
+};
 
 function LogicaSoma(index) {
-  // if (ArryaNum.indexOf("+") == 1) {
   if (typeof ArryaNum[index - 1] == "number") {
     ArraySoma.push(Number(ArryaNum[index - 1]));
-  }
+  };
 
   if (typeof ArryaNum[index + 1] == "number") {
     ArraySoma.push(Number(ArryaNum[index + 1]));
-  }
+  };
 
   Soma();
   IterarArrayNum();
-
-  // } else {
-  //   if (typeof ArryaNum[ArryaNum.indexOf("+") + 1] == "number") {
-  //     ArraySoma.push(Number(ArryaNum[ArryaNum.indexOf("+") + 1]));
-  //   };
-  //   Soma();
-  // };
-}
+};
 
 function Soma() {
   let counter = ArraySoma[0];
   for (let i = 1; i < ArraySoma.length; i++) {
     counter += ArraySoma[i];
-  }
-  // if (IndexArrayNum == 1) {
+  };
+ 
   ArryaNum.splice(0, 2);
   ArryaNum[0] = counter;
   ArraySoma.length = 0;
-  // } else {
-  //   ArryaNum.splice(IndexArrayNum - 1, 2);
-  //   ArryaNum[IndexArrayNum - 1] = counter;
-  //   ArraySoma.length = 0;
-  // };
-}
+};
 
 function LogicaSubtracao(index) {
-  // if (ArryaNum.indexOf("-") == 1) {
   if (typeof ArryaNum[index - 1] == "number") {
     ArraySubtracao.push(Number(ArryaNum[index - 1]));
-  }
+  };
 
   if (typeof ArryaNum[index + 1] == "number") {
     ArraySubtracao.push(Number(ArryaNum[index + 1]));
-  }
+  };
 
   Subtracao();
   IterarArrayNum();
-
-  // } else {
-  //   if (typeof ArryaNum[ArryaNum.indexOf("-") + 1] == "number") {
-  //     ArraySubtracao.push(Number(ArryaNum[ArryaNum.indexOf("-") + 1]));
-  //   };
-  //   Subtracao(ArryaNum.indexOf("-"));
-  // };
-}
+};
 
 function Subtracao() {
   let counter = ArraySubtracao[0];
   for (let i = 1; i < ArraySubtracao.length; i++) {
     counter -= ArraySubtracao[i];
-  }
-  // if (IndexArrayNum == 1) {
+  };
+
   ArryaNum.splice(0, 2);
   ArryaNum[0] = counter;
   ArraySubtracao.length = 0;
-  // } else {
-  //   ArryaNum.splice(IndexArrayNum - 1, 2);
-  //   ArryaNum[IndexArrayNum - 1] = counter;
-  //   ArraySubtracao.length = 0;
-  // };
-}
+};
